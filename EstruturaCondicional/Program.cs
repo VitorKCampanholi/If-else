73739bb80cdb7a3302eec55a274b2d3599889838
VKBOOY <VKBOOY@DESKTOP-DE9CVF9>
2024-11-02 17:14:57 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Quaal a hora atual ?");
+int hora = int.Parse(Console.ReadLine());
+
+if (hora < 12)
+{
+    Console.WriteLine("Bom dia!");
+}
+else if (hora < 18)
+{
+    Console.WriteLine("Boa Tarde");
+}
+else
+{
+    Console.WriteLine("Boa Noite");
+}
+
+{
+    
+}
